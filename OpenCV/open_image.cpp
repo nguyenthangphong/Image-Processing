02498f0fae05f11fd0a7d29f9287355da9c95074
nguyenthangphong <nthangphong2601@gmail.com>
2024-12-28 23:40:43 +0700

    Fix bug open image

--- OpenCV/open_image.cpp
+++ OpenCV/open_image.cpp
@@ -5,19 +5,22 @@ int main(void)
 {
     cv::Mat frame;
 
-    frame = cv::imread("Images/face_1.jpg");
+    frame = cv::imread("../Images/face_1.jpg");
 
     if (frame.empty())
     {
-        std::cerr << "Cound not open image !" << std::endl;
+        std::cerr << "Could not open image !" << std::endl;
         return -1;
     }
 
     cv::imshow("Image", frame);
 
-    if (cv::waitKey(1) == 'q')
+    while (true)
     {
-        exit(1);
+        if (cv::waitKey(30) == 'q')
+        {
+            break;
+        }
     }
 
     return 0;
